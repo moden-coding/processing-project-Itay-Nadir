d7fba8ef1bb05f79cf0e5647d5e39854f263aa58
Itay <28inadir@gannacademy.org>
2024-10-01 10:15:31 -0400

    commited

--- src/App.java
+++ src/App.java
@@ -2,10 +2,13 @@ import processing.core.*;
 
 public class App extends PApplet {
     float circle1X, circle1Y, circle2X, circle2Y;
-    float circle1Size = 30;
-    float circle2Size = 70;
-    float circle1Speed = 5;
-    float circle2Speed = 3;
+    float circle1Size = 20;
+    float circle2Size = 70; 
+    float jumpHeight = 150;
+    float circle1Velocity = 0;
+    boolean isJumping = false;
+    boolean collided = false;
+    float gravity = 3;
 
     public static void main(String[] args) {
         PApplet.main("App");
@@ -19,31 +22,52 @@ public class App extends PApplet {
     public void setup() {
         circle1X = 50;
         circle1Y = height / 2;
-        circle2X = 200;
+        circle2X = 800;
         circle2Y = height / 2;
-        
+
     }
-        public void draw() {
-        background(135,206,235);
+
+    public void draw() {
+        background(135, 206, 235);
         fill(255, 0, 0);
+
+        if (circle2X < 0) {
+            circle2X = width;
+        }
+        if (isJumping) {
+            circle1Y -= -5;
+            if (circle1Y <= height / 2 - jumpHeight) {
+                isJumping = false;
+            }
+
+        } else {
+            if (circle1Y < height / 2) {
+                circle1Y += gravity;
+            }
+        }
+
         ellipse(circle1X, circle1Y, circle1Size, circle1Size);
-        
 
-
-
-        if (circle2X > width) {
-            circle2X = 0;
+        if (collision()) {
+            circle2X = width / 2;
+            collided = true;
+        } else {
+            circle2X -= 2;
+            collided = false;
+        }
+        if (!collided) {
+            ellipse(circle2X, circle2Y, circle2Size, circle2Size);
         }
     }
-    public boolean collision(circle1, circle2){
-        double distanceofCircles = dist(circle1X, circle1Y, circle2X, circle2Y);
-        if(distanceofCircles <= (circle1 )){
-            return true;
-        }else{
-            return false;
+
+    public boolean collision() {
+        double distanceOfCircles = dist(circle1X, circle1Y, circle2X, circle2Y);
+        return distanceOfCircles <= (circle1Size / 2 + circle2Size / 2);
+    }
+
+    public void keyPressed() {
+        if (key == ' ' && !isJumping) {
+            isJumping = true;
         }
-        }
-    public void mousePressed() {
-        circle1X += circle1Speed;
-}
+    }
 }
